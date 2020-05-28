a435035d750e5b62a6c285716335b16c217c240c
Ali <parlakciali@gmail.com>
2020-05-28 21:40:19 +0300

    Update version to 1.7.0

--- script.py
+++ script.py
@@ -29,7 +29,7 @@ from src.utils import (GLOBAL, createLogFile, jsonFile, nameCorrector,
 
 __author__ = "Ali Parlakci"
 __license__ = "GPL"
-__version__ = "1.6.5"
+__version__ = "1.7.0"
 __maintainer__ = "Ali Parlakci"
 __email__ = "parlakciali@gmail.com"
 
